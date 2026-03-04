cc09f88dfc5562ae171cf078d6d207a30fa52e92
eowusu14 <owusu.boateng@flyzipline.com>
2026-03-04 01:48:00 +0000

    minor fixes

--- flake.nix
+++ flake.nix
@@ -23,8 +23,7 @@
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ pkgs.ghostty
-          pkgs.tmux
+        [ pkgs.tmux
           pkgs.obsidian
         ];
 
@@ -46,26 +45,26 @@
       };
 
       fonts.packages = [
-                (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-        ];
+        pkgs.nerd-fonts."jetbrains-mono"
+      ];
 
       system.activationScripts.applications.text = let
              env = pkgs.buildEnv {
                name = "system-applications";
                paths = config.environment.systemPackages;
-               pathsToLink = "/Applications";
+               pathsToLink = [ "/Applications" ];
              };
            in
              pkgs.lib.mkForce ''
                # Set up applications.
                echo "setting up /Applications..." >&2
                rm -rf /Applications/Nix\ Apps
-               mkdir -p /Applications/Nix\ Apps
                find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-               while read src; do
+               while IFS= read -r src; do
                  app_name=$(basename "$src")
                  echo "copying $src" >&2
-                 ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+                 rm -rf "/Applications/$app_name"
+                 ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/$app_name"
                done
              '';
 
@@ -73,7 +72,7 @@
              dock.autohide  = true;
              dock.largesize = 64;
              dock.persistent-apps = [
-               "${pkgs.obsidian}/Applications/Obsidian.app"
+               "/Applications/Obsidian.app"
                "/System/Applications/Calendar.app"
              ];
              finder.FXPreferredViewStyle = "clmv";
@@ -83,8 +82,7 @@
              NSGlobalDomain.KeyRepeat = 2;
            };
 
-        # Auto upgrade nix package and the daemon service.
-        services.nix-daemon.enable = true;
+        # Auto upgrade nix package.
         # nix.package = pkgs.nix;
 
 
@@ -101,6 +99,7 @@
       # Used for backwards compatibility, please read the changelog before changing.
       # $ darwin-rebuild changelog
       system.stateVersion = 6;
+      system.primaryUser = "owusu.boateng";
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
